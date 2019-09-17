39623d4d8010cc1960c163d2ef5d7cb390c24db7
ht123666 <634682378@qq.com>
2019-09-17 19:16:19 +0800

    111

--- src/ht/servlet/Servlet4.java
+++ src/ht/servlet/Servlet4.java
@@ -32,6 +32,7 @@ public class Servlet4 extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("进入servlet4:/");
+		System.out.println("11111");
 	}
 
 }

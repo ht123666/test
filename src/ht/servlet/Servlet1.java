db89e3cb2c623efb32d7e77766b579ffe9f60b6c
ht123666 <634682378@qq.com>
2019-09-17 15:09:50 +0800

    111

---
+++ src/ht/servlet/Servlet1.java
@@ -0,0 +1,22 @@
+package ht.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+public class Servlet1 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("进入servlet1:index.jsp");
+	}
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+</head>
+<body>
+	index页面
+<!-- 	<img alt="" src="xx.css"> -->
+<!-- 	<img alt="" src="aa.css"> -->
+</body>
+</html>

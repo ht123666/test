e45e10ccab0ede27aed14c6fa45e62e48e8fd5c1
ht123666 <54163861+ht123666@users.noreply.github.com>
2019-09-17 15:31:00 +0800

    Update Servlet1.java

--- src/ht/servlet/Servlet1.java
+++ src/ht/servlet/Servlet1.java
@@ -17,7 +17,7 @@ public class Servlet1 extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("进入servlet1:index.jsp");
-		System.out.println("222");
+		System.out.println("333");
 	}
 
 }
